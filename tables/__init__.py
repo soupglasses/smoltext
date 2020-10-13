1bcc772ec21e08bde27845b951596599e3149ebb
imsofi <6748208-imsofi@users.noreply.gitlab.com>
2020-10-13 02:07:36 +0200

    Initial commit

---
+++ tables/__init__.py
@@ -0,0 +1,13 @@
+from ._subscript import table_subscript
+from ._smallcaps import table_smallcaps
+from ._emoji import table_emoji
+from ._bold import table_bold
+from ._handwriting import table_handwriting
+from ._italic import table_italic
+from ._demonic import table_demonic
+from ._bold import table_bold
+from ._fancy import table_fancy
+
+__all__ = ['table_subscript', 'table_smallcaps', 'table_emoji', 'table_bold',
+           'table_handwriting', 'table_italic', 'table_demonic', 'table_bold',
+           'table_fancy']
