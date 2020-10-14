6ef3f43e2429a0fbe032e26504a9e64b6a299bf6
imsofi <6748208-imsofi@users.noreply.gitlab.com>
2020-10-15 01:16:57 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='smoltext',
-    version='1.2.0',
+    version='1.2.1',
     scripts=['smoltext'],
     author='Sofi',
     description='A font converter utility with the extra fonts included in utf8.',
