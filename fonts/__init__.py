d8f20b83a6f940fa06b1a6644b2ca528f6935487
imsofi <6748208-imsofi@users.noreply.gitlab.com>
2020-10-15 01:16:22 +0200

    Added back __all__ to __init__

--- fonts/__init__.py
+++ fonts/__init__.py
@@ -6,3 +6,14 @@ from ._handwriting import handwriting
 from ._italic import italic
 from ._demonic import demonic
 from ._fancy import fancy
+
+__all__ = [
+    'subscript',
+    'smallcaps',
+    'emoji',
+    'bold',
+    'handwriting',
+    'italic',
+    'demonic',
+    'fancy',
+]
