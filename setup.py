dab1afd84300dd14669e5bf53f96eff2ce462243
imsofi <6748208-imsofi@users.noreply.gitlab.com>
2020-10-24 19:56:06 +0200

    Update gitlab link

--- setup.py
+++ setup.py
@@ -5,18 +5,20 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='smoltext',
-    version='1.4.1',
+    version='1.4.2',
     scripts=['smoltext'],
     author='Sofi',
     description='A font converter utility with the extra fonts included in utf8.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://gitlab.com/imsofi/smoltext/',
     packages=setuptools.find_packages(),
+    project_urls={
+        'Source': 'https://gitlab.com/imsofi/smoltext/',
+    },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
 )
