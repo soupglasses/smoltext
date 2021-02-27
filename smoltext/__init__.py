cbb4b5569cad1e933313b7cc57b208f894161034
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2021-02-27 05:18:40 +0100

    Move package into dedicated module

---
+++ smoltext/__init__.py
@@ -0,0 +1,1 @@
+from .smoltext import main
---
+++ smoltext/smoltext.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import argparse
+import smoltext.fonts as fonts
+
+fonts = {
+    'subscript': fonts.subscript,
+    'emoji': fonts.emoji,
+    'smallcaps': fonts.smallcaps,
+    'bold': fonts.bold,
+    'handwriting': fonts.handwriting,
+    'italic': fonts.italic,
+    'demonic': fonts.demonic,
+    'fancy': fonts.fancy,
+    'monospace': fonts.monospace,
+}
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="A font utility to use the extra fonts included in utf8."
+    )
+    parser.add_argument(
+        'text', nargs='*',
+        help="text to process"
+    )
+    parser.add_argument(
+        '-c', '--copy', action="store_true",
+        help="copies processed text to clipboard"
+    )
+    parser.add_argument(
+        '-f', '--font', default='subscript', choices=fonts, metavar='FONT',
+        help="which font to use"
+    )
+    parser.add_argument(
+        '--list-fonts', action="store_true",
+        help="list all supported fonts"
+    )
+    args = parser.parse_args()
+
+    if args.list_fonts:
+        print(
+            "Fonts:",
+            *(f"{font.translate(fonts[font])} ({font})" for font in fonts),
+            sep='\n • ',
+        )
+        quit()
+
+    if args.text:
+        text = " ".join(args.text)
+    else:
+        text = input('> ')
+
+    print(text.translate(fonts[args.font]))
+
+
+if __name__ == '__main__':
+    main()
